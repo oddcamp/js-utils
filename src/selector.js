579fd6e5d3df37926777363efd81dd2644eac62d
Osvaldas Valutis <osvaldas.v@gmail.com>
2018-03-26 11:20:15 +0300

    Code getParents()

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,7 @@
 /*
-  Accepts string, Element, NodeList and returns Array of elements
+  Accepts String, Element, NodeList, Array and returns Array of elements
+
+  @param {String|Array|Element|NodeList} elements
 */
 
 const getElements = (elements) => {
@@ -22,7 +24,10 @@ const getElements = (elements) => {
 
 /*
   Based on how Element.closest() works. Returns true if `element` has the
-  closest ancestor (or itself) that matches the element|selector
+  closest ancestor (or itself) that matches the `matches` (element|selector)
+
+  @param {Element} element
+  @param {String|Element} matches
 */
 
 const hasClosest = (element, matches) => {
@@ -40,7 +45,46 @@ const hasClosest = (element, matches) => {
   }
 }
 
+/*
+  Returns an Array of parents of `element` that matches the given `selector`
+  up until the `until` matching element|selector
+
+  @param {Element} element
+  @param {String} selector [optional]
+  @param {String|Element} until [optional]
+*/
+
+const getParents = (element, selector = '', until = null) => {
+  let parents = []
+  element = element.parentNode
+
+  while(element) {
+    if(element === document) {
+      break
+    }
+
+    if(!selector || element.matches(selector)) {
+      parents.push(element)
+    }
+
+    if(until) {
+      if(typeof until === 'string') {
+        if(element.matches(until)) {
+          break
+        }
+      }
+      else if(element === until) {
+        break
+      }
+    }
+
+    element = element.parentNode
+  }
+  return parents
+}
+
 export {
   getElements,
   hasClosest,
+  getParents,
 }
