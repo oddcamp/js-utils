3002082f0f4092ef65fa9010613117063246d03d
Eduardo Nunes <ed@eduardonunes.me>
2018-08-10 14:45:27 +0100

    Fix getCookieValue return

--- src/cookie.js
+++ src/cookie.js
@@ -9,8 +9,11 @@
 
 const getCookieValue = (name) => {
   const value = "; " + document.cookie;
-	const parts = value.split("; " + name + "=");
-	if (parts.length == 2) return parts.pop().split(";").shift();
+  const parts = value.split("; " + name + "=");
+  if (parts.length == 2)
+    return parts.pop().split(";").shift();
+  else
+    return "";
 }
 
 /*
