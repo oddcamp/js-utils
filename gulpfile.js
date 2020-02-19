a3e5c6005d8d363b361f0c91330c458e8a5dc4ee
jnylen <me@jnylen.nu>
2020-02-19 16:41:41 +0100

    Bump all dependencies to the latest version

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,35 @@
-'use strict'
+"use strict";
 
-const gulp = require('gulp')
-const eslint = require('gulp-eslint')
-const childProcess = require('child_process')
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const childProcess = require("child_process");
 
-gulp.task('eslint', () => {
-  return gulp.src('./src/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-})
+gulp.task(
+  "eslint",
+  gulp.series(() =>
+    gulp
+      .src("./src/**/*.js")
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError())
+  )
+);
 
-gulp.task('watch', () => {
-  gulp.watch('./src/**/*.js', ['eslint'])
-})
+gulp.task(
+  "watch",
+  gulp.series(() => gulp.watch("./src/**/*.js", gulp.series("eslint")))
+);
 
-gulp.task('server', () => {
-  const cp = childProcess.spawn('node', ['./server.js'])
+gulp.task("server", () => {
+  const cp = childProcess.spawn("node", ["./server.js"]);
 
-  cp.stdout.on('data', function(data) {
+  cp.stdout.on("data", function(data) {
     console.log(data.toString());
   });
 
-  cp.stderr.on('data', function(data) {
+  cp.stderr.on("data", function(data) {
     console.log(data.toString());
   });
-})
+});
 
-gulp.task('default', ['watch', 'server'])
+gulp.task("default", gulp.series(gulp.parallel("watch", "server")));
