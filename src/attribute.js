1fe9c96932ed7a724d277cf897d537a0fb215cb7
Osvaldas Valutis <osvaldas.v@gmail.com>
2021-09-12 12:28:33 +0300

    feat: containsAnyClass & containsAllClasses

--- src/attribute.js
+++ src/attribute.js
@@ -18,6 +18,12 @@ const manipulateClass = (type, elements, classnames, force = undefined) => {
   })
 }
 
+const getElement = (element) => {
+  return typeof element === 'string'
+    ? document.querySelector(element)
+    : element
+}
+
 /*
   ------------------------------------------------------------------------------
   An extended implementation of Element.classList.add(): adds classname(s) to
@@ -60,10 +66,54 @@ const toggleClass = (...args) => {
 
 /*
   ------------------------------------------------------------------------------
+  Check if an element contains at least one classname
+
+  @param {String|Element} elements Selector, single element
+  @param {String} classnames Space separated if multiple
+*/
+
+const containsAnyClass = (element, classnames) => {
+  element = getElement(element)
+  if (!element) return null
+
+  let contains = false
+  classnames.split(` `).some((classname) => {
+    contains = element.classList.contains(classname)
+    return contains
+  })
+
+  return contains
+}
+
+/*
+  ------------------------------------------------------------------------------
+  Check if an element contains all classnames
+
+  @param {String|Element} elements Selector, single element
+  @param {String} classnames Space separated if multiple
+*/
+
+const containsAllClasses = (element, classnames) => {
+  element = getElement(element)
+  if (!element) return null
+
+  let contains = false
+  classnames.split(` `).every((classname) => {
+    contains = element.classList.contains(classname)
+    return contains
+  })
+
+  return contains
+}
+
+/*
+  ------------------------------------------------------------------------------
 */
 
 export {
   addClass,
   removeClass,
   toggleClass,
+  containsAnyClass,
+  containsAllClasses,
 }
